aeaa5c1164094b60eb3eb38d37842705b48f85fd
unknown <cctpmax@gmail.com>
2018-06-06 21:14:28 -0400

    Finished the Get function

--- MAC-Address-Lookup-Tool/Private/Get-MACPrefixList.ps1
+++ MAC-Address-Lookup-Tool/Private/Get-MACPrefixList.ps1
@@ -6,4 +6,30 @@ Function Get-MACPrefixList {
         [Switch]$DownloadNewCopy
     )
 
+    $InformationPreference = "Continue"
+    $PrefixListPath = "$PSScriptRoot\..\Resources\List.txt"
+
+    #Check if a copy of the file already exists
+    $File = Get-ChildItem $PrefixListPath -ErrorAction SilentlyContinue
+    $TestPath = $File | Test-Path
+    $OneWeekAgo = (Get-Date).AddDays(-7)
+
+    If ($TestPath -eq $False -or $File.LastWriteTime -lt $OneWeekAgo -or $DownloadNewCopy) {
+
+        Write-Information "Downloading a fresh copy of the prefix list..."
+
+        Download-MACPrefixList
+
+    }
+    
+    Else {
+
+        Write-Verbose "Utilizing the existing copy of the prefix list"
+
+    }
+
+    $PrefixList = Get-Content $PrefixListPath
+
+    Write-Output $PrefixList
+
 }
