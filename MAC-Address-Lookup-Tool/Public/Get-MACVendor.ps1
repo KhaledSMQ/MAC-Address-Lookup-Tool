3466902483a2387ba1714bc1876f35f6504c7d11
unknown <cctpmax@gmail.com>
2018-06-07 15:45:27 -0400

    Added the ability to look up MAC vendor by vendor name using -Vendor

--- MAC-Address-Lookup-Tool/Public/Get-MACVendor.ps1
+++ MAC-Address-Lookup-Tool/Public/Get-MACVendor.ps1
@@ -3,74 +3,92 @@ Function Get-MACVendor {
     [CmdletBinding()]
     Param (
 
-        [String]$MAC
+        [Parameter(Position=0,Mandatory=$True,ParameterSetName='LookupByMAC')]
+        [String]$MAC,
+
+        [Parameter(Position=0,Mandatory=$True,ParameterSetName='LookupByVendor')]
+        [String]$Vendor
 
     )
 
     $MACList = Parse-MACPrefixList
 
-    Switch -Regex ($MAC) {
+    Switch ($PSCmdlet.ParameterSetName) {
 
-        #Matches 123ABC format
-        '[A-Fa-f0-9]{6}' {
+        'LookupByMAC' {
 
-            #Nothing needs to be done, already in the correct format for where-object
+            Switch -Regex ($MAC) {
 
-        }
-
-        #Matches 123456ABCDEF format
-        '[A-Fa-f0-9]{12}' {
-
-            $MAC = $MAC.Substring(0,6)
-
-        }
+                #Matches 123ABC format
+                '[A-Fa-f0-9]{6}' {
         
-        #Matches 12.3A.BC format
-        '[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}' {
-
-            $MAC = $MAC.Replace(".","").Substring(0,6)
-
-        }
-
-        #Matches 12.34.56.AB.DC.EF format
-        '[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}' {
-
-            $MAC = $MAC.Replace(".","").Substring(0,6)
-
-        }
-
-        #Matches 12-3A-BC
-        '[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}' {
-
-            $MAC = $MAC.Replace("-","")
-
-        }
-
-        #Matches 12-34-56-AB-CD-EF
-        '[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}' {
+                    #Nothing needs to be done, already in the correct format for where-object
+        
+                }
+        
+                #Matches 123456ABCDEF format
+                '[A-Fa-f0-9]{12}' {
+        
+                    $MAC = $MAC.Substring(0,6)
+        
+                }
+                
+                #Matches 12.3A.BC format
+                '[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}' {
+        
+                    $MAC = $MAC.Replace(".","").Substring(0,6)
+        
+                }
+        
+                #Matches 12.34.56.AB.DC.EF format
+                '[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}' {
+        
+                    $MAC = $MAC.Replace(".","").Substring(0,6)
+        
+                }
+        
+                #Matches 12-3A-BC
+                '[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}' {
+        
+                    $MAC = $MAC.Replace("-","")
+        
+                }
+        
+                #Matches 12-34-56-AB-CD-EF
+                '[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}' {
+                    
+                    $MAC = $MAC.Replace("-","").Substring(0,6)
+        
+                }
+        
+                #Matches 1234-56AB-CDEF
+                '[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}' {
+        
+                    $MAC = $MAC.Replace("-","").Substring(0,6)
+        
+                }
+        
+                #Matches 1234.56AB.CDEF
+                '[A-Fa-f0-9]{4}.[A-Fa-f0-9]{4}.[A-Fa-f0-9]{4}' {
+        
+                    $MAC = $MAC.Replace(".","").Substring(0,6)
+        
+                }
+        
+            }
+        
+            $Output = ($MACList | Where-Object { $_.MACPrefix -eq $MAC })
             
-            $MAC = $MAC.Replace("-","").Substring(0,6)
-
         }
 
-        #Matches 1234-56AB-CDEF
-        '[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}' {
+        'LookupByVendor' {
 
-            $MAC = $MAC.Replace("-","").Substring(0,6)
-
-        }
-
-        #Matches 1234.56AB.CDEF
-        '[A-Fa-f0-9]{4}.[A-Fa-f0-9]{4}.[A-Fa-f0-9]{4}' {
-
-            $MAC = $MAC.Replace(".","").Substring(0,6)
-
+            $Output = $MACList | Where-Object { $_.MACVendor -eq $Vendor }
+            
         }
 
     }
 
-    $Vendor = ($MACList | Where-Object { $_.MACPrefix -eq $MAC })
-
-    Write-Output $Vendor
-
+    Write-Output $Output
+    
 }
