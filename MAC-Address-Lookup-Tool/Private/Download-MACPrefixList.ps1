ee49e138343fbef22378d9f77bccf4888c437f40
unknown <cctpmax@gmail.com>
2018-06-06 21:13:38 -0400

    A change to the Download function

--- MAC-Address-Lookup-Tool/Private/Download-MACPrefixList.ps1
+++ MAC-Address-Lookup-Tool/Private/Download-MACPrefixList.ps1
@@ -3,11 +3,14 @@ Function Download-MACPrefixList {
     [CmdletBinding()]
     Param (
         [Parameter(Position=0)]
-        $DownloadURL = 'https://linuxnet.ca/ieee/oui/nmap-mac-prefixes'
+        $DownloadURL = 'https://linuxnet.ca/ieee/oui/nmap-mac-prefixes',
+        $OutputFileName = 'List.txt'
     )
 
-    $Response = (Invoke-WebRequest -Uri $DownloadURL).Content
+    $Response = ((Invoke-WebRequest -Uri $DownloadURL).Content -split "`n")
 
-    $Response | Out-File $PSScriptRoot\..\Resources\List.txt
+    #$Response = $Response[0..($Response.Length-2)]
+
+    $Response | Out-File $PSScriptRoot\..\Resources\$OutputFileName
 
 }
