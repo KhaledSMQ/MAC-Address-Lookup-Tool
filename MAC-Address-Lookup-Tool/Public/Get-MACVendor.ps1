7767f1ac8c5688cda9f7038f8f03f77321ef0c03
unknown <cctpmax@gmail.com>
2018-06-07 15:52:42 -0400

    Added escape operators before the periods and dashes in the regular expressions to ensure they are considered literal characters and not considered regex operators

--- MAC-Address-Lookup-Tool/Public/Get-MACVendor.ps1
+++ MAC-Address-Lookup-Tool/Public/Get-MACVendor.ps1
@@ -48,28 +48,28 @@ Function Get-MACVendor {
                 }
         
                 #Matches 12-3A-BC
-                '[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}' {
+                '[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}' {
         
                     $MAC = $MAC.Replace("-","")
         
                 }
         
                 #Matches 12-34-56-AB-CD-EF
-                '[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}' {
+                '[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}-[A-Fa-f0-9]{2}' {
                     
                     $MAC = $MAC.Replace("-","").Substring(0,6)
         
                 }
         
                 #Matches 1234-56AB-CDEF
-                '[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}' {
+                '[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}' {
         
                     $MAC = $MAC.Replace("-","").Substring(0,6)
         
                 }
         
                 #Matches 1234.56AB.CDEF
-                '[A-Fa-f0-9]{4}.[A-Fa-f0-9]{4}.[A-Fa-f0-9]{4}' {
+                '[A-Fa-f0-9]{4}\.[A-Fa-f0-9]{4}\.[A-Fa-f0-9]{4}' {
         
                     $MAC = $MAC.Replace(".","").Substring(0,6)
         
