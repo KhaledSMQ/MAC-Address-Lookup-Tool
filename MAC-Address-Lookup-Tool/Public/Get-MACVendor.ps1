e12b2345b294c037df6a8c46b650710cf2e7df02
unknown <cctpmax@gmail.com>
2018-06-08 17:31:22 -0400

    Added pipeline and array support

--- MAC-Address-Lookup-Tool/Public/Get-MACVendor.ps1
+++ MAC-Address-Lookup-Tool/Public/Get-MACVendor.ps1
@@ -3,92 +3,110 @@ Function Get-MACVendor {
     [CmdletBinding()]
     Param (
 
-        [Parameter(Position=0,Mandatory=$True,ParameterSetName='LookupByMAC')]
-        [String]$MAC,
+        [Parameter(Position=0,Mandatory=$True,ParameterSetName='LookupByMAC',ValueFromPipeline=$True)]
+        [String[]]$MAC,
 
         [Parameter(Position=0,Mandatory=$True,ParameterSetName='LookupByVendor')]
         [String]$Vendor
 
     )
 
-    $MACList = Parse-MACPrefixList
+    Begin {
 
-    Switch ($PSCmdlet.ParameterSetName) {
+        $MACList = Parse-MACPrefixList
 
-        'LookupByMAC' {
+    }
 
-            Switch -Regex ($MAC) {
+    Process {
+    
+        Switch ($PSCmdlet.ParameterSetName) {
 
-                #Matches 123ABC format
-                '[A-Fa-f0-9]{6}' {
-        
-                    #Nothing needs to be done, already in the correct format for where-object
-        
-                }
-        
-                #Matches 123456ABCDEF format
-                '[A-Fa-f0-9]{12}' {
-        
-                    $MAC = $MAC.Substring(0,6)
-        
+            'LookupByMAC' {
+
+                ForEach ($Address in $MAC) {
+
+                    Switch -Regex ($Address) {
+
+                        #Matches 123ABC format
+                        '[A-Fa-f0-9]{6}' {
+                
+                            #Nothing needs to be done, already in the correct format for where-object
+                
+                        }
+                
+                        #Matches 123456ABCDEF format
+                        '[A-Fa-f0-9]{12}' {
+                
+                            $Address = $Address.Substring(0,6)
+                
+                        }
+                        
+                        #Matches 12.3A.BC format
+                        '[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}' {
+                
+                            $Address = $Address.Replace(".","").Substring(0,6)
+                
+                        }
+                
+                        #Matches 12.34.56.AB.DC.EF format
+                        '[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}' {
+                
+                            $Address = $Address.Replace(".","").Substring(0,6)
+                
+                        }
+                
+                        #Matches 12-3A-BC
+                        '[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}' {
+                
+                            $Address = $Address.Replace("-","")
+                
+                        }
+                
+                        #Matches 12-34-56-AB-CD-EF
+                        '[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}' {
+                            
+                            $Address = $Address.Replace("-","").Substring(0,6)
+                
+                        }
+                
+                        #Matches 1234-56AB-CDEF
+                        '[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}' {
+                
+                            $Address = $Address.Replace("-","").Substring(0,6)
+                
+                        }
+                
+                        #Matches 1234.56AB.CDEF
+                        '[A-Fa-f0-9]{4}\.[A-Fa-f0-9]{4}\.[A-Fa-f0-9]{4}' {
+                
+                            $Address = $Address.Replace(".","").Substring(0,6)
+                
+                        }
+                
+                    }
+                
+                    $Output = ($MACList | Where-Object { $_.MACPrefix -eq $Address })
+
+                    Write-Output $Output
+
                 }
                 
-                #Matches 12.3A.BC format
-                '[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}' {
-        
-                    $MAC = $MAC.Replace(".","").Substring(0,6)
-        
-                }
-        
-                #Matches 12.34.56.AB.DC.EF format
-                '[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}\.[A-Fa-f0-9]{2}' {
-        
-                    $MAC = $MAC.Replace(".","").Substring(0,6)
-        
-                }
-        
-                #Matches 12-3A-BC
-                '[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}' {
-        
-                    $MAC = $MAC.Replace("-","")
-        
-                }
-        
-                #Matches 12-34-56-AB-CD-EF
-                '[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}\-[A-Fa-f0-9]{2}' {
-                    
-                    $MAC = $MAC.Replace("-","").Substring(0,6)
-        
-                }
-        
-                #Matches 1234-56AB-CDEF
-                '[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}\-[A-Fa-f0-9]{4}' {
-        
-                    $MAC = $MAC.Replace("-","").Substring(0,6)
-        
-                }
-        
-                #Matches 1234.56AB.CDEF
-                '[A-Fa-f0-9]{4}\.[A-Fa-f0-9]{4}\.[A-Fa-f0-9]{4}' {
-        
-                    $MAC = $MAC.Replace(".","").Substring(0,6)
-        
-                }
-        
             }
-        
-            $Output = ($MACList | Where-Object { $_.MACPrefix -eq $MAC })
-            
-        }
 
-        'LookupByVendor' {
+            'LookupByVendor' {
 
-            $Output = $MACList | Where-Object { $_.MACVendor -eq $Vendor }
-            
+                $Output = $MACList | Where-Object { $_.MACVendor -eq $Vendor }
+
+                Write-Output $Output
+                
+            }
+
         }
 
     }
 
-    Write-Output $Output
+    End {
+
+    }
     
 }
