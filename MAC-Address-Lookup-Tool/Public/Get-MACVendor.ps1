00117556fc34ad595c2ebc247b9f3b3268db2d53
unknown <cctpmax@gmail.com>
2018-06-08 18:07:41 -0400

    Added commend based help to Get-MACVendor

--- MAC-Address-Lookup-Tool/Public/Get-MACVendor.ps1
+++ MAC-Address-Lookup-Tool/Public/Get-MACVendor.ps1
@@ -1,5 +1,59 @@
 Function Get-MACVendor {
 
+    <#
+    
+        .SYNOPSIS
+        Returns the vendor of a given MAC address, or returns the MAC prefix(s) for a given vendor
+
+        .DESCRIPTION
+        Takes an inputted MAC address, or MAC address prefix in a variety of different formats and returns
+        the assined vendor of the MAC address based on the OUI list published by the IEEE. It can also take
+        an inputted MAC address vendor, and return all of its assigned MAC address prefixes.
+
+        .PARAMETER MAC
+        The MAC(s) or MAC prefix(s) you wish to get the vendor name for. Accepts various different formats:
+         - 123ABC
+         - 123456ABCDEF
+         - 12.3A.BC
+         - 12.34.56.AB.DC.EF
+         - 12-3A-BC
+         - 12-34-56-AB-CD-EF
+         - 1234-56AB-CDEF
+         - 1234.56AB.CDEF
+
+        .PARAMETER Vendor
+        The name of the vendor you wish to recieve assined MAC prefixes for.
+
+        .EXAMPLE
+        PS> Get-MACVendor -MAC '0007B3','DC86D8','001377','94EB2C','B84FD5'
+        MACPrefix MACVendor
+        --------- ---------
+        0007B3    Cisco
+        DC86D8    Apple
+        001377    Samsung
+        94EB2C    Google
+        B84FD5    Microsoft
+
+        .EXAMPLE
+        PS> '0007B3','DC86D8','001377','94EB2C','B84FD5' | Get-MACVendor
+        MACPrefix MACVendor
+        --------- ---------
+        0007B3    Cisco
+        DC86D8    Apple
+        001377    Samsung
+        94EB2C    Google
+        B84FD5    Microsoft
+
+        .EXAMPLE
+        PS> Get-MACVendor -Vendor 'OnePlus'
+        MACPrefix MACVendor
+        --------- ---------
+        94652D    OnePlus
+        C0EEFB    OnePlus
+        64A2F9    OnePlus
+    
+    #>
+
     [CmdletBinding()]
     Param (
 
