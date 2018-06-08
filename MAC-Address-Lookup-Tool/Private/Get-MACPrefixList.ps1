828a434788ba1320e490db82da48eae0d5efacd5
unknown <cctpmax@gmail.com>
2018-06-08 17:54:05 -0400

    Added comment based help to Get-MACPrefixList

--- MAC-Address-Lookup-Tool/Private/Get-MACPrefixList.ps1
+++ MAC-Address-Lookup-Tool/Private/Get-MACPrefixList.ps1
@@ -1,5 +1,20 @@
 Function Get-MACPrefixList {
 
+    <#
+    
+        .SYNOPSIS
+        Returns the raw MAC prefix list
+
+        .DESCRIPTION
+        Returns the raw MAC prefix list. If the file does not exists, its runs 'Download-MACPrefixList'
+        to download it, then return the contents. If the file is older than a week, it will also 
+        download a new copy.
+
+        .PARAMETER DownloadNewCopy
+        Forces a download of the MAC prefix list regardless if it is stale
+    
+    #>
+
     [CmdletBinding()]
     Param (
         [Parameter(Position=0)]
