08c0dac4f53fc22f252947236d4269c301667774
unknown <cctpmax@gmail.com>
2018-06-08 17:51:14 -0400

    Added commend based help to Download-MACPrefixList

--- MAC-Address-Lookup-Tool/Private/Download-MACPrefixList.ps1
+++ MAC-Address-Lookup-Tool/Private/Download-MACPrefixList.ps1
@@ -1,5 +1,22 @@
 Function Download-MACPrefixList {
 
+    <#
+    
+        .SYNOPSIS
+        Downloads a copy of the MAC prefix list
+
+        .DESCRIPTION
+        Downloads a copy of the MAC prefix list from linuxnet.ca into a text file in the 
+        resources folder in the module root.
+
+        .PARAMETER DownloadURL
+        The URL to the MAC prefix list. Has a default value
+
+        .PARAMETER OutputFileName
+        The name of the file that will store the downloaded MAC prefix list
+
+    #>
+
     [CmdletBinding()]
     Param (
         [Parameter(Position=0)]
